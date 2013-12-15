367c1ee3a19730d8aeca0f1153cbe13ca9657ae1
Jordi Vermeulen <j.l.vermeulen@gmail.com>
2013-12-15 15:55:22 +0100

    Generic convolutions (actually correlations)

---
+++ Convolution.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+using System;
+
+namespace INFOIBV
+{
+    public static class Convolution // actually correlation
+    {
+        public static Color[,] Apply(Color[,] original, double[,] kernel)
+        {
+            Color[,] processed = new Color[original.GetLength(0), original.GetLength(0)];
+
+            for (int x = 0; x < original.GetLength(0); x++)
+                for (int y = 0; y < original.GetLength(1); y++)
+                {
+                    double[] value = new double[3];
+                    for (int i = kernel.GetLength(0) - 1; i >= 0; i--)
+                        for (int j = kernel.GetLength(1) - 1; j >= 0; j--)
+                        {
+                            // current pixel, extrapolate if necessary
+                            int pxx = Math.Max(Math.Min(x + i - kernel.GetLength(0) / 2, original.GetLength(0) - 1), 0);
+                            int pxy = Math.Max(Math.Min(y + j - kernel.GetLength(1) / 2, original.GetLength(1) - 1), 0);
+
+                            value[0] += kernel[i, j] * original[pxx, pxy].R;
+                            value[1] += kernel[i, j] * original[pxx, pxy].G;
+                            value[2] += kernel[i, j] * original[pxx, pxy].B;
+                        }
+                    for (int i = 0; i < value.Length; i++)
+                        value[i] = Math.Max(Math.Min(value[i], 255), 0);
+
+                    processed[x, y] = Color.FromArgb((byte)Math.Round(value[0]), (byte)Math.Round(value[1]), (byte)Math.Round(value[2]));
+                }
+
+            return processed;
+        }
+
+        public static Color[,] Apply(Color[,] original, double[] kernelx, double[] kernely)
+        {
+            Color[,] pass1 = new Color[original.GetLength(0), original.GetLength(0)];
+            Color[,] pass2 = new Color[original.GetLength(0), original.GetLength(0)];
+
+            for (int x = 0; x < original.GetLength(0); x++)
+                for (int y = 0; y < original.GetLength(1); y++)
+                {
+                    double[] value = new double[3];
+                    for (int i = kernelx.Length - 1; i >= 0; i--)
+                    {
+                        // current pixel, extrapolate if necessary
+                        int pxx = Math.Max(Math.Min(x + i - kernelx.Length / 2, original.GetLength(0) - 1), 0);
+
+                        value[0] += kernelx[i] * original[pxx, y].R;
+                        value[1] += kernelx[i] * original[pxx, y].G;
+                        value[2] += kernelx[i] * original[pxx, y].B;
+                    }
+                    for (int i = 0; i < value.Length; i++)
+                        value[i] = Math.Max(Math.Min(value[i], 255), 0);
+
+                    pass1[x, y] = Color.FromArgb((byte)Math.Round(value[0]), (byte)Math.Round(value[1]), (byte)Math.Round(value[2]));
+                }
+
+            for (int x = 0; x < original.GetLength(0); x++)
+                for (int y = 0; y < original.GetLength(1); y++)
+                {
+                    double[] value = new double[3];
+                    for (int i = kernelx.Length - 1; i >= 0; i--)
+                    {
+                        // current pixel, extrapolate if necessary
+                        int pxy = Math.Max(Math.Min(y + i - kernelx.Length / 2, original.GetLength(1) - 1), 0);
+
+                        value[0] += kernelx[i] * pass1[x, pxy].R;
+                        value[1] += kernelx[i] * pass1[x, pxy].G;
+                        value[2] += kernelx[i] * pass1[x, pxy].B;
+                    }
+                    for (int i = 0; i < value.Length; i++)
+                        value[i] = Math.Max(Math.Min(value[i], 255), 0);
+
+                    pass2[x, y] = Color.FromArgb((byte)Math.Round(value[0]), (byte)Math.Round(value[1]), (byte)Math.Round(value[2]));
+                }
+
+            return pass2;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace INFOIBV
+{
+    public partial class INFOIBV : Form
+    {
+        private Bitmap InputImage;
+        private Bitmap OutputImage;
+
+        public INFOIBV()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+           if (openImageDialog.ShowDialog() == DialogResult.OK) // Open File Dialog
+            {
+                string file = openImageDialog.FileName; // Get the file name
+                this.imageFileName.Text = file; // Show file name
+                if (this.InputImage != null)
+                    this.InputImage.Dispose(); // Reset image
+                this.InputImage = new Bitmap(file); // Create new Bitmap from file
+                if (this.InputImage.Size.Height <= 0 || this.InputImage.Size.Width <= 0 ||
+                    this.InputImage.Size.Height > 512 || this.InputImage.Size.Width > 512) // Dimension check
+                    MessageBox.Show("Error in image dimensions (have to be > 0 and <= 512)");
+                else
+                    this.pictureBox1.Image = this.InputImage; // Display input image
+            }
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            if (this.InputImage == null) // Get out if no input image
+                return;
+            if (this.OutputImage != null) // Reset output image
+                this.OutputImage.Dispose();
+            this.OutputImage = new Bitmap(this.InputImage.Size.Width, this.InputImage.Size.Height); // Create new output image
+            Color[,] Image = new Color[this.InputImage.Size.Width, this.InputImage.Size.Height]; // Create array to speed-up operations (Bitmap functions are very slow)
+
+            // Setup progress bar
+            this.progressBar.Visible = true;
+            this.progressBar.Minimum = 1;
+            this.progressBar.Maximum = InputImage.Size.Width * InputImage.Size.Height;
+            this.progressBar.Value = 1;
+            this.progressBar.Step = 1;
+
+            // Copy input Bitmap to array            
+            for (int x = 0; x < this.InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < this.InputImage.Size.Height; y++)
+                {
+                    Image[x, y] = this.InputImage.GetPixel(x, y); // Set pixel color in array at (x,y)
+                }
+            }
+
+            //==========================================================================================
+            // TODO: include here your own code
+            // example: create a negative image
+            //for (int x = 0; x < this.InputImage.Size.Width; x++)
+            //{
+            //    for (int y = 0; y < this.InputImage.Size.Height; y++)
+            //    {
+            //        Color pixelColor = Image[x, y]; // Get the pixel color at coordinate (x,y)
+            //        Color updatedColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B); // Negative image
+            //        Image[x, y] = updatedColor; // Set the new pixel color at coordinate (x,y)
+            //        this.progressBar.PerformStep(); // Increment progress bar
+            //    }
+            //}
+
+            Color[,] processed = Convolution.Apply(Image, SharpenKernel2D);
+            Image = processed;
+
+            //==========================================================================================
+
+            // Copy array to output Bitmap
+            for (int x = 0; x < this.InputImage.Size.Width; x++)
+            {
+                for (int y = 0; y < this.InputImage.Size.Height; y++)
+                {
+                    this.OutputImage.SetPixel(x, y, Image[x, y]); // Set the pixel color at coordinate (x,y)
+                }
+            }
+
+            this.pictureBox2.Image = this.OutputImage; // Display output image
+            this.progressBar.Visible = false; // Hide progress bar
+        }
+        
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (this.OutputImage == null) // Get out if no output image
+                return;
+            if (this.saveImageDialog.ShowDialog() == DialogResult.OK) // Save the output image
+                this.OutputImage.Save(this.saveImageDialog.FileName);
+        }
+
+        private static double[,] SmoothingKernel2D = new double[,] { { 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0 }, { 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0 }, { 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0 }, { 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0 }, { 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0, 1 / 25.0 } };
+        private static double[] SmoothingKernel1D = new double[] { 1 / 5.0, 1 / 5.0, 1 / 5.0, 1 / 5.0, 1 / 5.0 };
+        private static double[,] SharpenKernel2D = new double[,] { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } }; 
+    }
+}
