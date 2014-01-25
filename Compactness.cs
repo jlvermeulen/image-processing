e28b0c84909dd7c6c941168706b76cc5dd2ff889
Jordi Vermeulen <j.l.vermeulen@gmail.com>
2014-01-25 22:00:16 +0100

    Removing infinitely large objects

--- Compactness.cs
+++ Compactness.cs
@@ -14,32 +14,47 @@ namespace INFOIBV
             if (image[x, y] == 0)
                 x++;
 
+            bool[] startNeighbours = new bool[] { y > 0 && image[x, y - 1] != 0, x < w && image[x + 1, y] != 0, y < h && image[x, y + 1] != 0, x > 0 && image[x - 1, y] != 0 };
+
             StringBuilder builder = new StringBuilder();
             int direction = 1, xx = x, yy = y;
-            do
+            for (int k = 0; k < 4; k++) // annoying fix for when the perimeter passes through the startpoint more then once
             {
-                direction = (direction + 3) % 4; // one step counterclockwise from last direction
-                bool[] mayGoDirection = new bool[] { y > 0 && image[x, y - 1] != 0, x < w && image[x + 1, y] != 0, y < h && image[x, y + 1] != 0, x > 0 && image[x - 1, y] != 0 };
-                for (int i = 0; i < 4; i++)
+                if (!startNeighbours[k] || k != (direction + 3) % 4)
                 {
-                    // check possible directions in clockwise order
-                    if (mayGoDirection[(direction + i) % 4])
-                    {
-                        direction = (direction + i) % 4;
-                        builder.Append(direction);
-                        if (direction == 0)
-                            y--;
-                        else if (direction == 1)
-                            x++;
-                        else if (direction == 2)
-                            y++;
-                        else if (direction == 3)
-                            x--;
-                        break;
-                    }
+                    direction++;
+                    direction %= 4;
+                    continue;
                 }
+
+                do
+                {
+                    direction = (direction + 3) % 4; // one step counterclockwise from last direction
+                    bool[] mayGoDirection = new bool[] { y > 0 && image[x, y - 1] != 0, x < w && image[x + 1, y] != 0, y < h && image[x, y + 1] != 0, x > 0 && image[x - 1, y] != 0 };
+                    for (int i = 0; i < 4; i++)
+                    {
+                        // check possible directions in clockwise order
+                        if (mayGoDirection[(direction + i) % 4])
+                        {
+                            direction = (direction + i) % 4;
+                            builder.Append(direction);
+                            if (direction == 0)
+                                y--;
+                            else if (direction == 1)
+                                x++;
+                            else if (direction == 2)
+                                y++;
+                            else if (direction == 3)
+                                x--;
+                            break;
+                        }
+                    }
+
+                    if (xx == x && yy == y)
+                        startNeighbours[(direction + 2) % 4] = false; // remove incoming direction
+                }
+                while (xx != x || yy != y);
             }
-            while (xx != x || yy != y);
 
             return builder.ToString();
         }
